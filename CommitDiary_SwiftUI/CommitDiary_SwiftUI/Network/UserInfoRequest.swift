4f443317ed25d82e6d0e73782b0ca52df3110c49
horeng2 <huski.a.a@gmail.com>
2022-10-04 19:50:22 +0900

    feat: UserInfoRequest 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/UserInfoRequest.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/UserInfoRequest.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+struct UserInfoRequest {
+    let token: String
+    var url: URL? = URL(string: "https://api.github.com/user")
+    let httpMethod = "Get"
+    var headers: [String: String] {
+        ["Authorization": "token \(token)",
+         "Accept": "application/vnd.github.v3+json"]
+    }
+    
+    var urlRequest: URLRequest? {
+        guard let url = url else {
+            return nil
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = self.httpMethod
+        self.headers.forEach{ key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        return request
+    }
+}
