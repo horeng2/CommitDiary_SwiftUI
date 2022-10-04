f12f44b9ae9679ffd4d974c354efc88e7069ea2b
horeng2 <huski.a.a@gmail.com>
2022-10-04 19:51:56 +0900

    feat: LoginManager 타입 구현, requestCode, requestAccessToken 메소드 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/Utility/LoginManager.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/Utility/LoginManager.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+import UIKit
+
+struct LoginManager {
+    static let shared = LoginManager()
+    
+    private let clientId = "d7dbc87ea8c0b68452f7"
+    private let clientSecret = "9fa2a43c208619ab8f7fb0201b1e3445d565a6cc"
+    private let scope = "repo,user"
+    
+    func requestCode() -> URL {
+        let urlString = "https://github.com/login/oauth/authorize"
+        var components = URLComponents(string: urlString)!
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: self.clientId),
+            URLQueryItem(name: "scope", value: self.scope),
+        ]
+        
+        return components.url!
+    }
+    
+    func requestAccessToken(with code: String) {
+        let urlString = "https://github.com/login/oauth/access_token"
+        var components = URLComponents(string: urlString)!
+        components.queryItems = [
+            URLQueryItem(name: "client_id", value: self.clientId),
+            URLQueryItem(name: "client_secret", value: self.clientSecret),
+            URLQueryItem(name: "code", value: code)
+        ]
+        
+        var request = URLRequest(url: components.url!)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print(NetworkError.invaildData)
+                return
+            }
+            guard let json = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                print(NetworkError.parsingError(type: "JSON"))
+                return
+            }
+            if let parsedData = json as? [String: Any] {
+                let token = parsedData["access_token"] as! String
+                print(token)
+            }
+        }
+        .resume()
+    }
+}
