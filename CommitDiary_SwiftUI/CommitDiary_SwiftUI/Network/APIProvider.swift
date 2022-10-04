6d04c3ca0c4d536da43afb345fe4ee30fc629500
horeng2 <huski.a.a@gmail.com>
2022-10-04 19:51:01 +0900

    feat: APIProvider 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/APIProvider.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/APIProvider.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class APICaller {
+    private let session: URLSession
+    
+    init(session: URLSession = URLSession.shared) {
+        self.session = session
+    }
+    
+    func request(_ request: UserInfoRequest, completionHandler: @escaping (Result<UserInfo, NetworkError>) -> Void) {
+        guard let urlRequest = request.urlRequest else {
+            return
+        }
+        session.dataTask(with: urlRequest) { data, response, error in
+            guard let httpResponse = response as? HTTPURLResponse,
+                  (200...299).contains(httpResponse.statusCode) else {
+                      let errorCode = String(describing: response)
+                      return completionHandler(.failure(.statusCodeError(code: errorCode)))
+                  }
+            guard let data = data else {
+                return completionHandler(.failure(.invaildData))
+            }
+            
+            guard let userInfo = try? JSONDecoder().decode(UserInfo.self, from: data) else {
+                return
+            }
+            print(userInfo)
+            completionHandler(.success(userInfo))
+        }
+        .resume()
+    }
+}
