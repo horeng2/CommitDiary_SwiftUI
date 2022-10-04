08427098eac8aca4d5d2522bc51f7c1731c596f3
horeng2 <huski.a.a@gmail.com>
2022-10-04 17:57:29 +0900

    feat: 루트뷰에 environment 변수로 CoreDataStack의 context 전달

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/CommitDiary_SwiftUIApp.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/CommitDiary_SwiftUIApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct CommitDiary_SwiftUIApp: App {
+    let coreDataStack = CoreDataStack.shared
+    
     var body: some Scene {
         WindowGroup {
             RootTabView()
+                .environment(\.managedObjectContext, coreDataStack.context)
         }
     }
 }
