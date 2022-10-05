d900d569b9fa493965b2d78b0fce9d15654c91f2
horeng2 <huski.a.a@gmail.com>
2022-10-05 22:55:15 +0900

    feat: ContrivutionsRequest 타입 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/ContributionsRequest.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/Network/ContributionsRequest.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+struct ContributionsRequest {
+    let userId: String
+    private let scheme = "https"
+    private let host = "github.com"
+    private let path = "/users"
+    
+    var component: URLComponents {
+        var components = URLComponents()
+        components.scheme = scheme
+        components.host = host
+        components.path = path + "/\(userId)" + "/contributions"
+        
+        return components
+    }
+    
+    var urlReauest: URLRequest? {
+        guard let url = component.url else {
+            return nil
+        }
+        return URLRequest(url: url)
+    }
+}
