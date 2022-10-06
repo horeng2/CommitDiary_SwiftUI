c25b2dde04ba1c5be6174976d4c80aee49cc42d8
horeng2 <huski.a.a@gmail.com>
2022-10-07 01:15:06 +0900

    feat: Contribution.empty, CommitLevel.color() 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/Model/Contribution.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/Model/Contribution.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct Contribution {
     let date: Date
@@ -19,10 +20,35 @@ struct Contribution {
     }
 }
 
+extension Contribution {
+    static var empty: Contribution {
+        Contribution(
+            date: Date(),
+            commitCount: 0,
+            level: .zero
+        )
+    }
+}
+
 enum CommitLevel: Int, CaseIterable {
     case zero
     case one
     case two
     case three
     case four
+    
+    func color() -> Color {
+        switch self {
+        case .zero:
+            return Color("lv0")
+        case .one:
+            return Color("lv1")
+        case .two:
+            return Color("lv2")
+        case .three:
+            return Color("lv3")
+        case .four:
+            return Color("lv4")
+        }
+    }
 }
