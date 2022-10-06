c04f3071528943546c9f867dfb0311402ae4c266
horeng2 <huski.a.a@gmail.com>
2022-10-07 01:56:42 +0900

    feat: setCellsColor 메소드 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/View/ContributionService.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/View/ContributionService.swift
@@ -6,22 +6,57 @@
 //
 
 import Foundation
+import SwiftUI
 
 class ContributionService: ObservableObject {
     @Published var contributions = [Contribution]()
-    var userId: String
+    @Published var bestCommit = 0
     
     init() {
-        self.userId = ""
+        let userId = UserDefaults.standard.string(forKey: "userId") ?? ""
+        Task {
+            await loadContribution(with: userId)
+            bestCommitCount()
+        }
     }
     
-    func loadContriburion() async {
+    func loadContribution(with userId: String) async {
         let githubNetwork = GithubNetwork()
-        guard let contributions = try? await githubNetwork.getContributions(with: self.userId) else {
+        guard let contributions = try? await githubNetwork.getContributions(with: userId) else {
             return
         }
         DispatchQueue.main.async {
             self.contributions = contributions
         }
+    }
+    
+    
+    func bestCommitCount() {
+        DispatchQueue.main.async {
+            self.bestCommit = self.contributions.map{ $0.commitCount }.max() ?? 0
+        }
+    }
+    
+    func continuousCommitCount() {
+        
+    }
+
+    func setCellsColor(columnsCount: Int) -> [[Color]] {
+        guard let lastDate = contributions.last?.date else {
+            return []
+        }
+        let rows = 7
+        let blankCellCount = rows - Calendar.current.component(.weekday, from: lastDate)
+        
+        let cellCount = rows * columnsCount - blankCellCount
+        
+        let levels = contributions.suffix(cellCount).map{ $0.level }
+        var colors = [[Color]]()
+
+        for index in stride(from: 0, to: levels.count, by: rows) {
+            let splitedColors = levels[index..<Swift.min(index+rows, levels.count)].map{ $0.color() }
+            colors.append(splitedColors)
+        }
+        return colors
     }
 }
