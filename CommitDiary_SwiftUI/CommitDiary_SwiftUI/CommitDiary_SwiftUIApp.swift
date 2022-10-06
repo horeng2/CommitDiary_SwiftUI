88d53ef0d918941d3200af297b5c9111ebae09c6
horeng2 <huski.a.a@gmail.com>
2022-10-07 02:01:32 +0900

    refactor: 변경 타입 적용

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/CommitDiary_SwiftUIApp.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/CommitDiary_SwiftUIApp.swift
@@ -10,12 +10,12 @@ import SwiftUI
 @main
 struct CommitDiary_SwiftUIApp: App {
     @StateObject var userInfoService = UserInfoService()
+    @StateObject var contributionService = ContributionService()
     let coreDataStack = CoreDataStack.shared
     
     var body: some Scene {
         WindowGroup {
-            RootTabView()
-                .environmentObject(userInfoService)
+            RootTabView(contributionService: contributionService, userInfoService: userInfoService)
                 .environment(\.managedObjectContext, coreDataStack.context)
                 .task {
                     await userInfoService.loadUserInfo()
