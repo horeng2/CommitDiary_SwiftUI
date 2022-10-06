b4c8f3a210fdba54d16ca28932573835723bfaad
horeng2 <huski.a.a@gmail.com>
2022-10-07 01:32:10 +0900

    feat: CommitChart 구현

--- CommitDiary_SwiftUI/CommitDiary_SwiftUI/View/CommitChart.swift
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/View/CommitChart.swift
@@ -5,4 +5,29 @@
 //  Created by 서녕 on 2022/10/05.
 //
 
-import Foundation
+import SwiftUI
+
+struct CommitChart<Index: View>: View {
+    let rows = 7
+    let columns: Int
+    let spacing: CGFloat
+    let index: (Int, Int) -> Index
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: spacing) {
+            ForEach(0..<columns, id: \.self) { row in
+                VStack(alignment: .center, spacing: spacing) {
+                    ForEach(0..<rows, id: \.self) { column in
+                        index(column, row)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(columns: Int, spacing: CGFloat, @ViewBuilder index: @escaping (Int, Int) -> Index) {
+        self.columns = columns
+        self.spacing = spacing
+        self.index = index
+    }
+}
---
+++ CommitDiary_SwiftUI/CommitDiary_SwiftUI/View/ContributionView.swift
@@ -0,0 +1,20 @@
+//
+//  ContributionView.swift
+//  CommitDiary_SwiftUI
+//
+//  Created by 서녕 on 2022/10/07.
+//
+
+import SwiftUI
+
+struct ContributionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContributionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContributionView()
+    }
+}
